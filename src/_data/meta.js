fb57f2ddf5fbfcdb74970a9e3496998f89a3a26c
Si Jobling <simon.jobling@gmail.com>
2023-09-17 21:44:24 +0100

    Update meta data with UK

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,7 +2,7 @@ module.exports = {
   url: process.env.URL || "http://localhost:8080",
   siteName: "Unstyled",
   siteDescription:
-    "Studio producing podcasts and tech communities",
+    "UK studio producing podcasts and tech communities",
   authorName: "Si Jobling",
   twitterUsername: "Si", // no `@`
 };
